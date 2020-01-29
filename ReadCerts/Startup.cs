9ec2c77f3efd886328e59d8c58847d6c5828deaf
Faiz ShaikhFaizFfafa <fashaikh@microsoft.com>
2020-01-29 13:29:56 -0800

    Read private key from local cert

--- ReadCerts/Startup.cs
+++ ReadCerts/Startup.cs
@@ -8,11 +8,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace ReadCerts
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,6 +26,8 @@ namespace ReadCerts
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            _env = env;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -135,6 +141,56 @@ namespace ReadCerts
                 sb.Append($"Exception reading certs in 'LocalMachineMy':{ex.ToString()}<br/>");
 
             }
+            try
+            {
+                sb.Append($"<br/><br/><h2>Reading Private Key from pfx on disk /App_Data/</h2><br/>");
+                X509Certificate2 cert = new X509Certificate2
+                    (_env.ContentRootPath + "/App_Data/certsanddomaintests.pfx", File.ReadAllText(_env.ContentRootPath + "/App_Data/password.txt"),
+                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+
+            if (!cert.Subject.Contains("azure", StringComparison.OrdinalIgnoreCase) && !cert.Subject.Contains("FullOSTransport", StringComparison.OrdinalIgnoreCase))
+                        {
+                            try
+                            {
+                                sb.Append($"cert.Thumbprint: {cert.Thumbprint}<br/>");
+                            }
+                            catch { }
+                            try
+                            {
+                                sb.Append($"cert.HasPrivateKey: {cert.HasPrivateKey}<br/>");
+                            }
+                            catch { }
+                            try
+                            {
+                                AsymmetricAlgorithm key = cert.PrivateKey;
+                                sb.Append($"privateKey.KeySize: {key.KeySize}<br/>");
+                                sb.Append($"privateKey.SignatureAlgorithm: {key.SignatureAlgorithm}<br/>");
+                                sb.Append($"privateKey.KeyExchangeAlgorithm: {key.KeyExchangeAlgorithm}<br/>");
+                            }
+                            catch { }
+                            try
+                            {
+                                sb.Append($"cert.Issuer: {cert.Issuer}<br/>");
+                            }
+                            catch { }
+                            try
+                            {
+                                sb.Append($"cert.Subject: {cert.Subject}<br/>");
+                            }
+                            catch { }
+                            sb.Append($"==========================================<br/>");
+                        }
+                else
+                {
+                    sb.Append($"NO CERTS FOUND in 'LocalMachineMy' !!<br/>");
+                }
+            }
+            catch (Exception ex)
+            {
+                sb.Append($"Exception reading certs in 'LocalMachineMy':{ex.ToString()}<br/>");
+
+            }
+
             sb.Append("</body></html>");
             return sb.ToString();
         }
